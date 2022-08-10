c0aff43fc26117102b7bb5ff22e200885515756e
John <88310061+yeh-john@users.noreply.github.com>
2022-08-10 20:45:11 +0800

    Add files via upload

---
+++ pyautogui/linux/auto_del.py
@@ -0,0 +1,25 @@
+import pyautogui as pgui
+import time
+
+
+while True:
+    time.sleep(3)
+    pgui.moveTo(x=891, y=571)
+    time.sleep(2)
+    pgui.scroll(-200)
+    time.sleep(2)
+    pgui.scroll(-200)
+    time.sleep(2)
+    pgui.click(x=882, y=943)
+    time.sleep(2)
+    pgui.click(x=882, y=943)
+    time.sleep(2)
+    pgui.moveTo(x=891, y=571)
+    time.sleep(2)
+    pgui.scroll(-100)
+    time.sleep(2)
+    pgui.click(x=895, y=915)
+    time.sleep(2)
+    pgui.click(x=1029, y=613)
+    time.sleep(2)
+    pgui.click(x=710, y=165)
